0a5ad72295d60b8a3fba50332a8e63cc084b13af
Italo Leal <itlcoorporativo@gmail.com>
2023-11-20 00:28:23 -0300

    Fundamentos: Cria a estrutura do projeto

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="p-8 bg-slate-900 text-slate-100 h-screen">
+      <h1 className="font-bold text-5xl">Hello world!</h1>
+      <h2>Hello world!</h2>
+      <p>Hello world!</p>
+    </div>
+
+  )
+}
