e396282bde586173bba0d86b69e0e00154214d0e
Italo Leal <itlcoorporativo@gmail.com>
2023-11-28 12:05:40 -0300

    Fundamentos: Dark Mode no Tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 export default function Home() {
   return (
-    <div className="p-8 bg-slate-900 text-slate-100 h-screen">
-      <h1 className="font-bold text-3xl sm:text-5xl lg:text-6xl flex items-center gap-3 before:w-0.5 before:h-8 before:bg-sky-500">
-        Hello Taiwind!
-      </h1>
-      <h2>Hello world!</h2>
-      <p>Hello world!</p>
-      <button className="bg-sky-500 px-4 py-2 rounded font-medium mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed" disabled>
-        Sign In
-      </button>
+    <div className="p-8 bg-slate-50 text-slate-900 dark:bg-slate-900 dark:text-slate-100 h-screen flex flex-col items-center text-center">
+      <div className="max-w-2xl">
+        <h1 className="font-bold text-3xl sm:text-4xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </h1>
+        <p className="mt-2 dark:text-slate-400">Amet et itaque distinctio illum eius provident cupiditate necessitatibus nulla numquam reprehenderit dolor explicabo nisi</p>
+        <button className="bg-sky-500 dark:bg-sky-400 dark:text-sky-950 text-white px-4 py-2 rounded font-medium mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed">
+          Sign In
+        </button>
+      </div>
     </div>
   )
 }
