78c00e923d64eb3994a9037af9ade0c39fcb34f4
Italo Leal <itlcoorporativo@gmail.com>
2023-11-28 11:21:34 -0300

    Fundamentos: Seletores e Estados

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 export default function Home() {
   return (
     <div className="p-8 bg-slate-900 text-slate-100 h-screen">
-      <h1 className="font-bold text-5xl">Hello world!</h1>
+      <h1 className="font-bold text-5xl flex items-center gap-3 before:w-0.5 before:h-8 before:bg-sky-500">
+        Hello world!
+      </h1>
       <h2>Hello world!</h2>
       <p>Hello world!</p>
+      <button className="bg-sky-500 px-4 py-2 rounded font-medium mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed" disabled>
+        Sign In
+      </button>
     </div>
 
   )
