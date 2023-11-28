a70026744f13f62cf75bffdf91ec454e2b1862e7
Italo Leal <itlcoorporativo@gmail.com>
2023-11-28 11:45:16 -0300

    Fundamentos: Responsividade e Breakpoints

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div className="p-8 bg-slate-900 text-slate-100 h-screen">
-      <h1 className="font-bold text-5xl flex items-center gap-3 before:w-0.5 before:h-8 before:bg-sky-500">
-        Hello world!
+      <h1 className="font-bold text-3xl sm:text-5xl lg:text-6xl flex items-center gap-3 before:w-0.5 before:h-8 before:bg-sky-500">
+        Hello Taiwind!
       </h1>
       <h2>Hello world!</h2>
       <p>Hello world!</p>
@@ -10,6 +10,5 @@ export default function Home() {
         Sign In
       </button>
     </div>
-
   )
 }
