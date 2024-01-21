118264c5eb477aa6c379c03f753f72674eb87d40
Italo Leal <itlcoorporativo@gmail.com>
2024-01-21 20:52:14 -0300

    Input de biografia

--- src/app/components/Form/Select/index.tsx
+++ src/app/components/Form/Select/index.tsx
@@ -4,14 +4,14 @@ import { ReactNode } from 'react'
 import * as SelectPrimitive from '@radix-ui/react-select'
 import { ChevronDown } from 'lucide-react'
 
-export interface SelectProps {
+export interface SelectProps extends SelectPrimitive.SelectProps {
   children: ReactNode
   placeholder: string
 }
 
-export function Select({ children, placeholder }: SelectProps) {
+export function Select({ children, placeholder, ...props }: SelectProps) {
   return (
-    <SelectPrimitive.Root>
+    <SelectPrimitive.Root {...props}>
       <SelectPrimitive.Trigger className="flex h-11 w-full justify-between gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm data-[placeholder]:text-zinc-600">
         <SelectPrimitive.Value
           placeholder={placeholder}
