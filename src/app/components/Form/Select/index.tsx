1c8bf363085c15af55cb1ef017d787994d15ea7e
Italo Leal <itlcoorporativo@gmail.com>
2024-01-22 18:16:00 -0300

    Animando abertura dos Selects

--- src/app/components/Form/Select/index.tsx
+++ src/app/components/Form/Select/index.tsx
@@ -27,7 +27,7 @@ export function Select({ children, placeholder, ...props }: SelectProps) {
           side="bottom"
           position="popper"
           sideOffset={8}
-          className="z-10 w-[--radix-select-trigger-width] overflow-hidden rounded-lg border border-zinc-300 bg-white"
+          className="animate-slideDownAndFade z-10 w-[--radix-select-trigger-width] overflow-hidden rounded-lg border border-zinc-300 bg-white"
         >
           <SelectPrimitive.Viewport>{children}</SelectPrimitive.Viewport>
         </SelectPrimitive.Content>
