6ce616668e7e824c5903fe55ebe13df666c8f737
Italo Leal <itlcoorporativo@gmail.com>
2024-02-02 00:49:20 -0300

    Tema dark da Sidebar

--- src/app/components/Sidebar/Profile.tsx
+++ src/app/components/Sidebar/Profile.tsx
@@ -11,8 +11,10 @@ export function Profile() {
       />
 
       <div className="flex flex-col truncate">
-        <span className="text-sm font-semibold text-zinc-700">Italo Leal</span>
-        <span className="truncate text-sm text-zinc-500">
+        <span className="text-sm font-semibold text-zinc-700 dark:text-zinc-100">
+          Italo Leal
+        </span>
+        <span className="truncate text-sm text-zinc-500 dark:text-zinc-400">
           itlcoorporativo@gmail.com
         </span>
       </div>
